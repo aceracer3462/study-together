09a77c5cd234a5636adbcf4c1494c90ecbedcfe0
Darin Tang <arceracer.3462@gmail.com>
2022-04-03 14:51:17 -0700

    first commit

---
+++ src/main/HomePage.js
@@ -0,0 +1,18 @@
+
+import * as React from 'react';
+
+export default class HomePage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      "Hello World!"
+    );
+  }
+}
